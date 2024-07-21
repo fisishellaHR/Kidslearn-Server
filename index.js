4fdad93c45fdd6083deacef9408232c275c45378
fisi <hegihayagi34@gmail.com>
2024-07-21 16:42:49 +0700

    ngetest

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ import { AdminRouter } from "./routes/Admin.js";
 import { ModulesRouterHTML } from "./routes/ModulesHTML.js";
 import { ModulesRouterCSS } from "./routes/ModulesCSS.js";
 
-dotenv.config();
 const app = express();
+dotenv.config({ path: "./config/.env" });
 
-app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -20,13 +19,29 @@ app.use(
     credentials: true,
   })
 );
+
+app.options("*", cors());
+app.use(express.json());
 app.use(cookieParser());
 
-app.use("/auth", UserRouter);
-app.use("/auth/:id", UserRouter);
-app.use("/admin", AdminRouter);
-app.use("/module", ModulesRouterHTML);
-app.use("/moduledua", ModulesRouterCSS);
-mongoose.connect("mongodb://127.0.0.1:27017/authentication");
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+});
+
+app.use("/api/auth", UserRouter);
+app.use("/api/auth/:id", UserRouter);
+app.use("/api/admin", AdminRouter);
+app.use("/api/module", ModulesRouterHTML);
+app.use("/api/moduledua", ModulesRouterCSS);
+mongoose
+  .connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+  });
